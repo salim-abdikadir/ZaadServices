afeeeca8a0db39cc6e0a36ffa6c005a00ef107c3
Salim abdikadir <salimabdikadir2845@gmail.com>
2025-01-30 14:01:23 +0300

    edited the last message

---
+++ db.js
@@ -0,0 +1,82 @@
+const User = require("./UserModel");
+exports = [
+  new User("1234", "1234", "Salim Abdikadir Mohamed", "10000"),
+  new User(
+    "4456321",
+    "1234",
+    "Mohamed Farah Liibaan",
+    Math.floor(Math.random() * 30)
+  ),
+  new User(
+    "6456321",
+    "1234",
+    "Mohamed Farah Liibaan",
+    Math.floor(Math.random() * 30)
+  ),
+  new User(
+    "4455321",
+    "1234",
+    "Mohamed Farah Liibaan",
+    Math.floor(Math.random() * 30)
+  ),
+  new User(
+    "3223321",
+    "1234",
+    "Mohamed Farah Liibaan",
+    Math.floor(Math.random() * 30)
+  ),
+  new User(
+    "3333221",
+    "1234",
+    "Mohamed Farah Liibaan",
+    Math.floor(Math.random() * 30)
+  ),
+  new User(
+    "4256321",
+    "1234",
+    "Mohamed Farah Liibaan",
+    Math.floor(Math.random() * 30)
+  ),
+  new User(
+    "3856331",
+    "1234",
+    "Mohamed Farah Liibaan",
+    Math.floor(Math.random() * 30)
+  ),
+  new User(
+    "8856321",
+    "1234",
+    "Mohamed Farah Liibaan",
+    Math.floor(Math.random() * 30)
+  ),
+  new User(
+    "7563218",
+    "1234",
+    "Mohamed Farah Liibaan",
+    Math.floor(Math.random() * 30)
+  ),
+  new User(
+    "6476329",
+    "1234",
+    "Mohamed Farah Liibaan",
+    Math.floor(Math.random() * 30)
+  ),
+  new User(
+    "3256321",
+    "1234",
+    "Mohamed Farah Liibaan",
+    Math.floor(Math.random() * 30)
+  ),
+  new User(
+    "3456321",
+    "1234",
+    "Mohamed Farah Liibaan",
+    Math.floor(Math.random() * 30)
+  ),
+  new User(
+    "3256321",
+    "1234",
+    "Mohamed Farah Liibaan",
+    Math.floor(Math.random() * 30)
+  ),
+];
